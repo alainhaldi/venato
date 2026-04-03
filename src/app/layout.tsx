38e734685a7eaa234c8001e8c5b8d6ae79955342
alain <alain.haldi@pm.me>
2026-04-03 10:22:08 +0200

    Add ThemeSwitcher

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import AppHeader from "@/components/header";
+import { ThemeProvider } from "@/components/theme/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,14 +26,17 @@ export default function RootLayout({
 }>) {
   return (
     <html
+      suppressHydrationWarning
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body>
-        <AppHeader></AppHeader>
-        <main className="min-h-screen flex-1 overflow-y-auto overflow-x-hidden py-12 px-8 bg-secondary/10 flex flex-col">
-          {children}
-        </main>
+        <ThemeProvider>
+          <AppHeader></AppHeader>
+          <main className="min-h-screen flex-1 overflow-y-auto overflow-x-hidden py-12 px-8 bg-secondary/30 flex flex-col">
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,11 @@
+import { ThemeSwitcher } from "./theme/theme-switcher";
+
 const AppHeader = () => {
   return (
     <div className="sticky top-0 z-50 flex flex-row h-12 items-center justify-between px-4 border-b backdrop-blur-xs">
       <p>Button</p>
       <p className="text-2xl font-bold">venato</p>
-      <p>theme switcher</p>
+      <ThemeSwitcher></ThemeSwitcher>
     </div>
   );
 };
