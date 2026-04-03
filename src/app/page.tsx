98aad6fc2240c43b67dbdf8cf8c613ace6a7b53e
alain <alain.haldi@pm.me>
2026-04-03 15:30:24 +0200

    Improve ProjectBar Layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ProjectBar from "@/features/projects/components/project-bar";
 
 export default function Home() {
   return (
-    <div className="bg-amber-700">
+    <div className="flex flex-col items-center ">
       <ProjectBar></ProjectBar>
       <p>hello</p>
     </div>
--- src/features/projects/components/project-bar.tsx
+++ src/features/projects/components/project-bar.tsx
@@ -3,10 +3,10 @@ import IconButton from "@/components/icon-button";
 
 const ProjectBar = () => {
   return (
-    <>
-      <div>ProjectBar</div>
+    <div className="flex w-full justify-between max-w-2xl pb-4 border-b">
+      <p className="text-2xl">Projects</p>
       <IconButton icon={<LucidePackagePlus />} label="New Project"></IconButton>
-    </>
+    </div>
   );
 };
 
