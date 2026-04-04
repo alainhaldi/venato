85772c11723cb4c01b87707de758cc1f30bb2dea
alain <alain.haldi@pm.me>
2026-04-04 17:29:44 +0200

    Add ProjectCardComponent with Template Code

--- src/app/projects/home/page.tsx
+++ src/app/projects/home/page.tsx
@@ -1,12 +1,16 @@
 import ProjectBar from "@/features/projects/components/project-bar";
+import ProjectCard from "@/features/projects/components/project-card";
 
-const ProjectsHome = () => {
+const ProjectsHomePage = () => {
+  const data = [1, 2, 3, 4];
   return (
     <div className="flex flex-col items-center ">
       <ProjectBar></ProjectBar>
-      <p>hello</p>
+      {data.map((item) => (
+        <ProjectCard key={item}></ProjectCard>
+      ))}
     </div>
   );
 };
 
-export default ProjectsHome;
+export default ProjectsHomePage;
